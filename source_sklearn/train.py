70b27fdf833e70c2dc3bd30f970b6564fc19b8cb
Rahul <rahulatrkm@gmail.com>
2019-12-21 10:34:53 +0530

    Completed coding part

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.externals import joblib
 
 ## TODO: Import any additional libraries you need to define a model
-
+from sklearn.svm import LinearSVC
 
 # Provided model load function
 def model_fn(model_dir):
@@ -15,20 +15,20 @@ def model_fn(model_dir):
     in the main if statement.
     """
     print("Loading model.")
-    
+
     # load using joblib
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     print("Done loading model.")
-    
+
     return model
 
 
 ## TODO: Complete the main code
 if __name__ == '__main__':
-    
+
     # All of the model parameters and training parameters are sent as arguments
     # when this script is executed, during a training job
-    
+
     # Here we set up an argument parser to easily access the parameters
     parser = argparse.ArgumentParser()
 
@@ -37,9 +37,9 @@ if __name__ == '__main__':
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    
+
     ## TODO: Add any additional arguments that you will need to pass into your model
-    
+
     # args holds all passed-in arguments
     args = parser.parse_args()
 
@@ -50,21 +50,21 @@ if __name__ == '__main__':
     # Labels are in the first column
     train_y = train_data.iloc[:,0]
     train_x = train_data.iloc[:,1:]
-    
-    
-    ## --- Your code here --- ##
-    
 
-    ## TODO: Define a model 
-    model = None
-    
-    
+
+    ## --- Your code here --- ##
+
+
+    ## TODO: Define a model
+    model = LinearSVC()
+
+
     ## TODO: Train the model
-    
-    
-    
+    model.fit(train_x, train_y)
+
+
     ## --- End of your code  --- ##
-    
+
 
     # Save the trained model
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
